34744cad3af65d5c2c1bb6dc07cf8138b1609466
RoboDexo2000 <150604236+RoboDexo2000[bot]@users.noreply.github.com>
2023-11-18 08:43:44 +0000

    fix(container): update image ghcr.io/siderolabs/kubelet to v1.28.4

--- tofu/talos_cluster/bootstrap.tf
+++ tofu/talos_cluster/bootstrap.tf
@@ -12,7 +12,7 @@ resource "talos_machine_configuration_apply" "controlplanes" {
       machine = {
         kubelet = {
           # renovate: datasource=docker depName=ghcr.io/siderolabs/kubelet
-          image                               = "ghcr.io/siderolabs/kubelet:v1.28.2"
+          image                               = "ghcr.io/siderolabs/kubelet:v1.28.4"
           defaultRuntimeSeccompProfileEnabled = true
           disableManifestsDirectory           = true
           extraArgs = {
