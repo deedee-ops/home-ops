ec0455e6d19a4d29465bacd16bfdc848a1000bed
Igor Rzegocki <igor@rzegocki.pl>
2023-12-28 19:55:42 +0100

    feat(opentofu): remove cloud config in favor of own s3

--- opentofu/cloudflare/variables.tf
+++ opentofu/cloudflare/variables.tf
@@ -1,3 +1,8 @@
+variable "base_domain" {
+  description = "Domain used for all homelab needs"
+  type        = string
+}
+
 variable "cloudflare_account_id" {
   description = "Cloudflare Account ID"
   type        = string
@@ -14,9 +19,4 @@ variable "cloudflare_tunnel_secret" {
   description = "Cloudflare tunnel secret (at least 32 bytes, base64 encoded)"
   type        = string
   sensitive   = true
-}
-
-variable "base_domain" {
-  description = "Domain used for all homelab needs"
-  type        = string
 }
