da40ba91f58a756f8d478de0cb1e1a26fb2d33ad
Igor Rzegocki <igor@rzegocki.pl>
2024-01-04 20:55:13 +0100

    feat(synology): add zot registry

--- kubernetes/apps/home-iot/synology/files/syncer.sh
+++ kubernetes/apps/home-iot/synology/files/syncer.sh
@@ -3,9 +3,12 @@
 mkdir -p /dest/acme-sh || true
 mkdir -p /dest/borgmatic/borgmatic.d || true
 mkdir -p /dest/borgmatic/config || true
+mkdir -p /dest/zot/config || true
+mkdir -p /dest/zot/registry || true
 
 cp /src/borgmatic-homelab.yaml /dest/borgmatic/borgmatic.d/homelab.yaml
 cp /src/borgmatic-private.yaml /dest/borgmatic/borgmatic.d/private.yaml
+cp /src/zot-config.json /dest/zot/config/config.json
 cp /src/docker-compose.yaml /dest/docker-compose.yml
 
 trap : TERM INT; sleep infinity & wait
