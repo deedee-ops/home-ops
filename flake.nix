00e881512a238284c4b006797dee135a845bf6b4
Igor Rzegocki <igor@rzegocki.pl>
2024-03-26 22:06:01 +0100

    feat(nix): introduce reproducible environment with flake.nix

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.age
+              pkgs.go-task
+              pkgs.kubernetes-helm
+              pkgs.jq
+              pkgs.lefthook
+              pkgs.opentofu
+              pkgs.sops
+              pkgs.yq-go
+
+              # linters
+              pkgs.kubeconform
+              pkgs.shellcheck
+              pkgs.tflint
+              pkgs.yamllint
+            ];
+          };
+        }
+      );
+}
