2244b2be2bac3d22f2754d91873fddc979aa7261
Igor Rzegocki <igor@rzegocki.pl>
2024-09-15 12:00:06 +0200

    chore: pin digests

--- nix/machines/supervisor/modules/arion-adguard-home.nix
+++ nix/machines/supervisor/modules/arion-adguard-home.nix
@@ -8,7 +8,7 @@
         # by default DNS is set to adguard in docker which causes internal loopback and resolving problems
         # that's why we need to force it to actual gateway
         dns = [ "10.42.1.1" ];
-        image = "ghcr.io/deedee-ops/adguardhome:v0.107.52";
+        image = "ghcr.io/deedee-ops/adguardhome:v0.107.52@sha256:755c513b878bba45454624e477d9eb0d11d1738e03fb86220ca85ba21451ca45";
         ports = [
           "53:53/tcp"
           "53:53/udp"
