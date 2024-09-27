ecc542a62b417329674ad03b02e3bbae2c619343
Igor Rzegocki <igor@rzegocki.pl>
2024-09-27 16:48:09 +0200

    fix(nix): ignore attic push errors

--- nix/modules/attic-client.nix
+++ nix/modules/attic-client.nix
@@ -2,7 +2,7 @@
 {
   system.activationScripts = {
     push-to-attic.text = ''
-      ${pkgs.attic-client}/bin/attic push homelab $(ls -d --color=never /nix/store/*/ | grep -v fake_nixpkgs)
+      ${pkgs.attic-client}/bin/attic push homelab $(ls -d --color=never /nix/store/*/ | grep -v fake_nixpkgs) || true
     '';
   };
 
