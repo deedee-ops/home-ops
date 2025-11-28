3e7565855cb3b302371306ccf8ec56ad2b6c2116
Igor Rzegocki <igor@rzegocki.pl>
2025-11-28 12:54:42 +0100

    feat: add mail-archive

---
+++ kubernetes/apps/default/mail-archive/resources/imapsync.sh
@@ -0,0 +1,15 @@
+imapsync \
+  --host1 imap.migadu.com \
+  --user1 "$(cat /secrets/SOURCE_USER)" \
+  --passfile1 "/secrets/SOURCE_PASS" \
+  --ssl1 \
+  --host2 "mail-archive-dovecot" \
+  --user2 "$(cat /secrets/DESTINATION_USER)" \
+  --passfile2 "/secrets/DESTINATION_PASS" \
+  --usecache \
+  --exclude "INBOX|Sent|Drafts|Junk|Trash" \
+  --nofoldersizes \
+  --tmpdir "/data" \
+  --logdir "/data" \
+  --logfile "log.txt" \
+  --pidfile "/data/imapsync.pid"
