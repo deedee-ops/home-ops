22110cd5c4ed120a6ac9143c9fc1cac0a4d66058
Igor Rzegocki <igor@rzegocki.pl>
2025-12-09 20:56:45 +0100

    chore: clean up yaml json schemas

--- scripts/lint-yaml-language-server.sh
+++ scripts/lint-yaml-language-server.sh
@@ -18,16 +18,16 @@ check_extension() {
   return 1
 }
 
-# Load .ylsignore patterns into an array
-load_ylsignore_patterns() {
-  local ignore_file="$1"
+# Load list from file to array
+load_patterns() {
+  local input_file="$1"
   local -n _patterns=$2
 
   while IFS= read -r line || [[ -n "$line" ]]; do
     # Skip empty lines and comments
     [[ -z "$line" || "$line" =~ ^[[:space:]]*# ]] && continue
     _patterns+=("$line")
-  done < "$ignore_file"
+  done < "$input_file"
 }
 
 # Check if a file matches any .ylsignore pattern
@@ -52,12 +52,40 @@ is_ignored() {
   return 1
 }
 
+# Validate URL
+validate_url() {
+  local url="$1"
+  local found_urlpat=0
+  for urlpat in "${url_patterns[@]}"; do
+    if [[ "$url" == "$urlpat"* ]]; then
+      found_urlpat=1
+      break
+    fi
+  done
+  if [ $found_urlpat -eq 0 ]; then
+    echo "❌ \$schema URL is not allowed pattern, in $doc_number of $file"
+    return 1
+  else
+    if [ -n "${YLS_RESOLVE_URLS}" ]; then
+      if [[ "$(curl -o /dev/null -s -w "%{http_code}" "$url")" != "200" ]]; then
+        echo "❌ \$schema URL is not working in $doc_number of $file"
+        return 1
+      fi
+    fi
+  fi
+
+  return 0
+}
+
 # Check a YAML file's documents
 check_file() {
   local file="$1"
+  shift
+  local url_patterns=("$@")
   local doc=""
   local doc_number=0
   local has_error=0
+
 
   [[ -d "$file" ]] && return 0
 
@@ -68,6 +96,10 @@ check_file() {
         if ! grep -q "^# yaml-language-server" <<< "$doc"; then
           echo "❌ Missing 'yaml-language-server' in document $doc_number of $file"
           has_error=1
+        else
+          url="$(grep "^# yaml-language-server" <<< "$doc" | awk -F= '{print $NF}')"
+          validate_url "$url"
+          has_error=$?
         fi
       fi
       doc=""
@@ -81,6 +113,10 @@ check_file() {
     if ! grep -q "^# yaml-language-server" <<< "$doc"; then
       echo "❌ Missing 'yaml-language-server' in document $doc_number of $file"
       has_error=1
+    else
+      url="$(grep "^# yaml-language-server" <<< "$doc" | awk -F= '{print $NF}')"
+      validate_url "$url"
+      has_error=$?
     fi
   fi
 
@@ -88,6 +124,10 @@ check_file() {
     if ! grep -q "^# yaml-language-server" "$file"; then
       echo "❌ Missing 'yaml-language-server' in document 1 of $file"
       has_error=1
+    else
+      url="$(grep "^# yaml-language-server" "$file" | awk -F= '{print $NF}')"
+      validate_url "$url"
+      has_error=$?
     fi
   fi
 
@@ -98,19 +138,28 @@ check_file() {
 check_all_yaml_files() {
   local working_dir="."
   local root_dir="$1"
-  local ignore_file="${working_dir}/.ylsignore"
+  local ylsconfig="${working_dir}/.ylsconfig"
   local -a ignore_patterns=()
+  local -a url_patterns=()
   local any_errors=0
 
-  if [[ -f "$ignore_file" ]]; then
-    load_ylsignore_patterns "$ignore_file" ignore_patterns
+  if [[ -f "$ylsconfig" ]]; then
+    ignore_file="$(mktemp)"
+    python -c "import json; d=json.load(open('${ylsconfig}')); [print(x) for x in d.get('ignoreFiles', [])]" > "$ignore_file"
+    load_patterns "$ignore_file" ignore_patterns
+    rm -f "$ignore_file"
+
+    url_file="$(mktemp)"
+    python -c "import json; d=json.load(open('${ylsconfig}')); [print(x) for x in d.get('allowOnlyURLs', [])]" > "$url_file"
+    load_patterns "$url_file" url_patterns
+    rm -f "$url_file"
   fi
 
   # If the root_dir is a directory, recursively find YAML files
   if [[ -d "$root_dir" ]]; then
     while IFS= read -r -d '' file; do
       if ! is_ignored "$file" "$working_dir" "${ignore_patterns[@]}"; then
-        if ! check_file "$file"; then
+        if ! check_file "$file" "${url_patterns[@]}"; then
           any_errors=1
         fi
       fi
@@ -119,7 +168,7 @@ check_all_yaml_files() {
   # If it's a file, process it directly
   elif [[ -f "$root_dir" ]]; then
     if ! is_ignored "$root_dir" "$working_dir" "${ignore_patterns[@]}"; then
-      if ! check_file "$root_dir"; then
+      if ! check_file "$root_dir" "${url_patterns[@]}"; then
         any_errors=1
       fi
     fi
@@ -130,7 +179,7 @@ check_all_yaml_files() {
   shift  # Shift to handle the case when there are additional files
   for file in "$@"; do
     if ! is_ignored "$file" "$working_dir" "${ignore_patterns[@]}"; then
-      if ! check_file "$file"; then
+      if ! check_file "$file" "${url_patterns[@]}"; then
         any_errors=1
       fi
     fi
@@ -149,9 +198,9 @@ if [[ "$#" -gt 1 ]]; then
 fi
 
 if check_all_yaml_files "$ROOT_DIR" "$@"; then
-  echo "✅ All YAML documents contain 'yaml-language-server'."
+  echo "✅ All YAML documents contain correct 'yaml-language-server'."
   exit 0
 else
-  echo "❗ Some YAML documents are missing 'yaml-language-server'."
+  echo "❗ Some YAML documents are missing correct 'yaml-language-server'."
   exit 1
 fi
