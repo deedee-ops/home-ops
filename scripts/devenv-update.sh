889e3c0ea223e0979fc4b3ba84fe0550d5922ad8
Igor Rzegocki <igor@rzegocki.pl>
2026-09-13 18:32:44 +0200

    feat: kill attic in favor of ncps

--- scripts/devenv-update.sh
+++ scripts/devenv-update.sh
@@ -6,5 +6,6 @@ cd "${SCRIPT_DIR}/.." && devenv update && devenv shell true
 
 # shellcheck disable=SC2044
 for profile in $(find "${SCRIPT_DIR}/../.devenv/profiles" -maxdepth 1 -mindepth 1 -type d); do
-  attic push nixlab "$(readlink -f "${profile}/profile")"
+  nix copy --to "https://nix.ajgon.casa/upload?compression=zstd" \
+    "$(readlink -f "${profile}/profile")"
 done
